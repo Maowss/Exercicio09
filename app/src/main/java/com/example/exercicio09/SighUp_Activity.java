21e1700a656d13aa69c1e6a32ede0d408536e9a5
Maowss <Maowss@gmail.com>
2019-05-29 16:22:27 -0300

    Todas as telas do exercicio 9 estão organizadas + Falta colocar as cores e ToolBar + Vincular as Paginas entre si

---
+++ app/src/main/java/com/example/exercicio09/SighUp_Activity.java
@@ -0,0 +1,13 @@
+package com.example.exercicio09;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SighUp_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sigh_up_layout);
+    }
+}
